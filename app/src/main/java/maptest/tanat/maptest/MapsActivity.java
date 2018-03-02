772d0375055e061ec87dfad2eb33cd13b11f9450
Pahan-Titan <pashok.pashkevich1995@gmail.com>
2018-03-02 17:08:10 +0200

    Update MapsActivity.java
    Implemented a search for the coordinates of the device and display of the marker on the map

--- app/src/main/java/maptest/tanat/maptest/MapsActivity.java
+++ app/src/main/java/maptest/tanat/maptest/MapsActivity.java
@@ -1,99 +1,115 @@
 package maptest.tanat.maptest;
 
-import android.graphics.Point;
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap map;
     SupportMapFragment mapFragment;
+    private LocationManager locationManager;
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                init();
+            }
+        });
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        init();
+    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private void init() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000 * 10, 10, locationListener);
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 1000 * 10, 10,
+                locationListener);
+    }
+
     @Override
     public void onMapReady(final GoogleMap map) {
-/*        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
-
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         map.getUiSettings().setZoomControlsEnabled(true);
-
-        String title = "This is Title";
-        String subTitle = "This is \nSubtitle";
-
-        //Marker
-        MarkerOptions markerOpt = new MarkerOptions();
-        markerOpt.position(new LatLng(Double.valueOf(0), Double.valueOf(0)))
-                .title(title)
-                .snippet(subTitle)
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.event_red));
-
-        //Set Custom InfoWindow Adapter
-        CustomInfoWindowAdapter adapter = new CustomInfoWindowAdapter(maptest.tanat.maptest.MapsActivity.this);
-        map.setInfoWindowAdapter(adapter);
-
-        map.addMarker(markerOpt);
-
-        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener()
-        {
-            @Override
-            public boolean onMarkerClick(Marker marker)
-            {
-                // get position marker
-                Double lat = marker.getPosition().latitude;
-                Double lng = marker.getPosition().longitude;
-
-                //center camera
-                map.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(lat, lng)));
-
-                // set position info window
-                marker.setInfoWindowAnchor(0.5f, 5.3f);
-
-                //show info window
-                marker.showInfoWindow();
-                return true;
-            }
-        });
-
+        this.map = map;
     }
+
+    public void showMarker(LatLng latLng) {
+        map.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("I'm here!"));
+    }
+
+    public double getLatitude(Location location) {
+        if (location != null) {
+            return location.getLatitude();
+        }
+        return 0;
+    }
+
+    public double getLongitude(Location location) {
+        if (location != null) {
+            return location.getLongitude();
+        }
+        return 0;
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            LatLng latLng = new LatLng(getLatitude(location), getLongitude(location));
+            showMarker(latLng);
+            map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
 }
